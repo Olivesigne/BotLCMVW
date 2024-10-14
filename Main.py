11dc634adb57180c0c6434dbd5399183e7e70ef9
Olivesigne <leonelponthenier@gmail.com>
2024-10-14 17:52:48 +0200

    Commandes: - /mp - /message Events: on_message: - Compte - Début Pub

--- Main.py
+++ Main.py
@@ -1,7 +1,10 @@
+import os
 from dataclasses import replace
 
 import discord
 from discord.ext import commands
+from jinja2.utils import open_if_exists
+from sympy import false
 
 token = "MTI3NjE0MzYwNjUzNjg2Mzc4Ng.GCUmTf.pdtFm9h3yFYP7QBzD2PnTgXera_C66CMMdaKc0"
 intents = discord.Intents.all()
@@ -19,40 +22,29 @@ async def on_ready():
     print(f"{len(synced)} commande(s) synchronisée(s)")
 
 
-@bot.tree.command(name="bonjour")
+@bot.tree.command(name="bonjour", description="Le bot te dit bonjour")
 async def bonjour(interaction: discord.Interaction):
     await interaction.response.send_message(f"Bonjour ! {interaction.user}")
 
 
-@bot.command()
-async def list(ctx):
-    await ctx.send(
-        "Liste des commandes : \n- !bonjour \n  - Le bot te dit bonjour \n \n- !list \n  - La liste des commandes \n \n- !info \n  - Ping tout le monde \n \n- !graphistes \n  - Ping les graphistes \n \n- !devs \n  - Ping les développeurs \n \n- !goat \n  - Ping le goat (Ne pas abuser) \n \n- !idee \n  - Ping tout le mode et les gens doivent réagir \n \n- !invite nom métier  \n  - Demande aux gens si la personne peut inviter quelqu'un \n- !reseaux \n - Affiche les réseaux du jeu")
+@bot.tree.command(name="list", description="Donne la liste des commandes disponibles")
+async def list(interaction: discord.Interaction):
+    await interaction.response.send_message("Liste de mes commandes:\n- /bonjour\n  - Le bot te dit bonjour\n- /list\n  - Ce que tu vois\n- /graphistes\n  - Ping les graphistes (ne pas abuser)\n- /devs\n  - Ping les devs (ne pas abuser)\n- /goat\n  - Ping le goat (ne surtout pas abuser)\n- /reseaux\n  - Affiche les réseaux du jeu")
 
 
-@bot.command()
-async def info(ctx):
-    await ctx.send("@everyone, \n")
+@bot.tree.command(name="graphistes", description="Ping les graphistes (l'abus est sanctionné)")
+async def graphistes(interaction: discord.Interaction, arg: str):
+    await interaction.response.send_message("<@&1276277931714412557>, \n" + arg)
 
 
-@bot.command()
-async def graphistes(ctx, arg):
-    await ctx.send("<@&1276277931714412557>, \n" + arg)
+@bot.tree.command(name="devs", description="Ping les devs (l'abus est sanctionné)")
+async def devs(interaction: discord.Interaction, arg: str):
+    await interaction.response.send_message("<@&1276277118250254378>, \n" + arg)
 
 
-@bot.command()
-async def devs(ctx, arg):
-    await ctx.send("<@&1276277118250254378>, \n" + arg)
-
-
-@bot.command()
-async def goat(ctx, arg):
-    await ctx.send("<@&1276276439892889651>, \n" + arg)
-
-
-@bot.command()
-async def idee(ctx):
-    await ctx.send("@everyone ✔ ou ❌ :point_up: ! Réagissez !")
+@bot.tree.command(name="goat", description="Ping le goat (l'abus est sanctionné)")
+async def goat(interaction: discord.Interaction, arg: str):
+    await interaction.response.send_message("<@&1276276439892889651>, \n" + arg)
 
 
 @bot.command()
@@ -63,29 +55,98 @@ async def compterendu(ctx):
         "        \n[Clôture] \n	Affichage du logo LifeCraft: My Virtual World avec un fond coloré et énergique. \n	Voix Off: Téléchargez LifeCraft: My Virtual World aujourd hui, et commencez votre nouvelle vie virtuelle. \n||@here||")
 
 
-@bot.command()
-async def reseaux(ctx):
-    await ctx.send("Les réseaux du jeu : https://bit.ly/m/Life-Craft-My-Virtual-World \n Allez y faire un tour ! 😉")
+@bot.tree.command(name="reseaux", description="Affiche les réseaux du jeu")
+async def reseaux(interaction: discord.Interaction):
+    await interaction.response.send_message("Les réseaux du jeu : https://bit.ly/m/Life-Craft-My-Virtual-World \n Allez y faire un tour ! 😉")
 
 
-@bot.command()
-async def ok(ctx):
-    await ctx.send(f"Je suis là, merci de demander {ctx.author} ! 😁")
+@bot.tree.command(name="ok", description="Demande si le bot est en marche")
+async def ok(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Je suis là, merci de demander {interaction.user.mention} ! 😁")
 
+@bot.tree.command(name="aider", description="Rejoins l'équipe du jeu")
+async def aider(interaction: discord.Interaction):
+    interaction.response.send_message(f"Si tu veux nous rejoindre {interaction.user.mention}, remplis ce formulaire : https://forms.gle/QDU5R8H3jTzHnNtbA et demande au staff de regarder ton profil dans un ticket ensuite ! :grin:")
 
-@bot.command()
-async def mp(ctx, member: discord.Member, *message):
+@bot.tree.command(name="mp", description="Envoie un message dans un salon spécifié (admins seulement)")
+async def mp(interaction: discord.Interaction, member: discord.Member, message: str):
     magnifiqueid = 1157663620671488001
     ledevprimeid = 1128719599811178577
-    idUser = ctx.author.id
+    idUser = interaction.user.id
     if idUser == magnifiqueid or idUser == ledevprimeid:
         chars = '(\'\',)'
-        await ctx.send(f"Un mp à bien été envoyé à {member.display_name} de ma part pour {ctx.author}")
-        print(message)
+        await interaction.response.send_message(f"Un mp à bien été envoyé à {member.display_name} de ma part pour {interaction.user.display_name}")
         translatedMessage = str(message).translate(str.maketrans('', '', chars))
-        print(translatedMessage)
         await member.send(f"Salut {member.mention}, \n{translatedMessage}")
     else:
-        await ctx.send("Désolé, vous n'avez pas la permission d'exécuter cette commande")
+        await interaction.response.send_message("Désolé, vous n'avez pas la permission d'exécuter cette commande :man_shrugging:")
+
+
+@bot.tree.command(name="message", description="Envoie un message dans un salon spécifié (admins seulement)")
+async def message(interaction: discord.Interaction, channel: discord.TextChannel, mention: discord.Role, message: str):
+    magnifiqueid = 1157663620671488001
+    ledevprimeid = 1128719599811178577
+    idUser = interaction.user.id
+    if idUser == magnifiqueid or idUser == ledevprimeid:
+        chars = '(\'\',)'
+        translatedMessage = str(message).translate(str.maketrans('', '', chars))
+        await channel.send(f"{mention.mention}, \n{translatedMessage}")
+    else:
+        await interaction.response.send_message("Désolé, vous n'avez pas la permission d'exécuter cette commande :man_shrugging:")
+
+@bot.event
+async def on_message(message):
+    idUser = int(message.author.id)
+    lcmvwb = 1276143606536863786
+    canalMessage = message.channel.id
+    compteChannel = 1295122739471843379
+    magnifiqueid = 1157663620671488001
+    ledevprimeid = 1128719599811178577
+    if canalMessage == compteChannel and idUser != lcmvwb:
+        fichierCompteRead = open("compte.txt", "r")
+        fichierCompteAppend = open("compte.txt", "a")
+        nblignes = 0
+        for line in fichierCompteRead:
+            nblignes += 1
+        nbline1 = nblignes + 1
+        #derniereligne = str(fichierCompteRead.readlines()[int(nblignes)])
+        fichierCompteRead.close()
+        if int(message.content) == nbline1: #and message.author.id in derniereligne == false:
+            thumbsup = '\N{THUMBS UP SIGN}'
+            await message.add_reaction(thumbsup)
+            fichierCompteAppend.write('\n' + message.content)
+        else:
+            await message.delete()
+    if 'https://' in message.content and idUser != lcmvwb and idUser != ledevprimeid and idUser != magnifiqueid:
+        await message.delete()
+        await message.channel.send(f"🛑 Pas de pub ici {message.author.mention} ! 🛑")
+        if os.path.exists("data" + str(message.author) + ".txt"):
+            fichierAppend = open("data" + str(message.author) + ".txt", "a")
+            fichierRead = open("data" + str(message.author) + ".txt", "r")
+            nblignes = 1
+            for line in fichierRead:
+                nblignes += 1
+            fichierAppend.write('\n' + f"pub(s): {int(nblignes)}")
+        else:
+            fichierCreate = open("data" + str(message.author) + ".txt", "x")
+            fichierAppend = open("data" + str(message.author) + ".txt", "a")
+            fichierRead = open("data" + str(message.author) + ".txt", "r")
+            nblignes = 1
+            for line in fichierRead:
+                nblignes += 1
+            fichierAppend.write(f"pub(s): {int(nblignes)}")
+
+    #if "https://" or "http://" in str(message.content) and idUser != lcmvwb:
+        #await message.delete()
+        #await message.channel.send(f"🛑 Pas de pub ici {message.author.mention} ! 🛑")
+        #fichierCreate = open("data" + str(message.author) + ".txt", "x")
+        #fichierAppend = open("data" + str(message.author) + ".txt", "a")
+        #fichierRead = open("data" + str(message.author) + ".txt", "r")
+        #nblignes = 1
+        #for line in fichierRead:
+            #nblignes += 1
+        #print(nblignes)
+        #print(int(nblignes))
+        #fichierAppend.write(f"pub(s): {int(nblignes)}")
 
 bot.run(token)
